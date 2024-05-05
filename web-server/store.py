04ca13a7c6ee4124a0eea9a330a1ca85bfd2b8d7
Diego Arango <yodaad@gmail.com>
2024-05-05 10:10:32 -0500

    Store file updated

--- web-server/store.py
+++ web-server/store.py
@@ -1,5 +1,9 @@
+"""Description: This file contains the store logic for the web server. It is a simple function that makes a GET request to an API and prints the response. 
+The main.py file imports this function and calls it when the server is run. This separation of concerns allows for better organization and modularity in the codebase."""
+
 import requests
 
+# Function to get categories from the API and print them
 def get_categories():
     r = requests.get(" https://api.escuelajs.co/api/v1/categories")
     print(r.status_code)
