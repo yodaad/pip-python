368c10906d236330d172fb3393d5c66371b358e7
Diego Arango <yodaad@gmail.com>
2024-05-08 06:24:38 -0500

    Main file updated

--- web-server/main.py
+++ web-server/main.py
@@ -1,18 +1,22 @@
+# Web Server with FastAPI and HTMLResponse 
+
 import store
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+# Define the route 
 @app.get("/")
 def get_list():
     return [1, 2, 3]
 
+# Define the route and the response class as HTMLResponse to return HTML content
 @app.get("/contact", response_class = HTMLResponse)
 def get_list():
     return """
-        <h1>Hello, this is done with HTML</h1>
-        <p>And I was done with HTML as well</p>
+        <h1>Hello, my name is Diego Arango</h1>
+        <p>And I am a software engineer</p>
     """
 
 def run():
